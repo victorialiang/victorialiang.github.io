03a164268a22bf3b7d638c4c5d85aebd2241b719
Victoria Liang <victoril@andrew.cmu.edu>
2018-11-29 18:42:35 -0500

    moved to this folder

---
+++ assets/js/scripts.js
@@ -0,0 +1,7 @@
+// $(document).ready(function(){
+// 	$(".project").hover(function(){
+// 		$(this).css("background-color","gray");},
+// 		function(){
+// 			$(this).css("background-color", "white");
+// 		});
+// 	});
