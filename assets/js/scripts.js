199aa617d9ea9c28d9fd8b4dd245f25092bf5d9c
Victoria Liang <victoril@andrew.cmu.edu>
2018-11-29 21:41:38 -0500

    hamburger

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,3 +5,13 @@
 // 			$(this).css("background-color", "white");
 // 		});
 // 	});
+
+function myFunction() {
+            var x = document.getElementById("nav");
+            if (x.className === "nav") {
+                x.className += " responsive";
+            } else {
+                x.className = "nav";
+            }
+        }
+      </scrip
