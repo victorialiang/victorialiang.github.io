d90aa757abde68720a29cbeaa99398279d937053
Victoria Liang <victoril@andrew.cmu.edu>
2018-11-30 01:00:35 -0500

    added responsiveness

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,12 +6,29 @@
 // 		});
 // 	});
 
-function myFunction() {
-            var x = document.getElementById("nav");
-            if (x.className === "nav") {
-                x.className += " responsive";
-            } else {
-                x.className = "nav";
-            }
-        }
-      </scrip
+// function myFunction() {
+//             var x = document.getElementById("nav");
+//             if (x.className === "nav") {
+//                 x.className += " responsive";
+//             } else {
+//                 x.className = "nav";
+//             }
+//         }
+//       </script>
+
+// $(".icon").click(function() {
+//   	x = 
+// 	removeFromCart($(this));
+//     $(this).parent().remove();
+//   });
+
+window.onload = function() {    
+	var x =document.getElementById("nav");    
+	x.onclick =function() {    
+		if (x.className === "nav") {
+            x.className += " responsive";
+        } else {
+            x.className = "nav";
+        }  
+	}
+}
